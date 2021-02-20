c52f63e02e886d474315fa90d87f8522d9f9d899
Krystian Jarmul <krystian.jarmul@ericsson.com>
2021-02-20 17:08:35 +0100

    Add dual laser

--- test_weapon.py
+++ test_weapon.py
@@ -23,3 +23,9 @@ def test_explosion_attributes():
     explosion = weapon.Explosion()
     assert explosion.directions == [1, 1, 1, 1, 0, 1, 1, 1, 1]
     assert explosion.range == [1, 1, 1, 1, 0, 1, 1, 1, 1]
+
+
+def test_dual_laser_attributes():
+    dual_laser = weapon.DualLaser()
+    assert dual_laser.directions == [0, 0, 0, 1, 0, 1, 0, 0, 0]
+    assert dual_laser.range == [0, 0, 0, 5, 0, 5, 0, 0, 0]
--- weapon.py
+++ weapon.py
@@ -35,3 +35,11 @@ class Explosion(Weapon):
         super().__init__()
         self.directions = [1, 1, 1, 1, 0, 1, 1, 1, 1]
         self.range = [1, 1, 1, 1, 0, 1, 1, 1, 1]
+
+
+class DualLaser(Weapon):
+
+    def __init__(self):
+        super().__init__()
+        self.directions = [0, 0, 0, 1, 0, 1, 0, 0, 0]
+        self.range = [0, 0, 0, 5, 0, 5, 0, 0, 0]
