6809801fd53304033c366d2c29fda9b8fdc33109
Krystian Jarmul <krystian.jarmul@ericsson.com>
2021-02-20 11:17:10 +0100

    Add hard body

--- body.py
+++ body.py
@@ -11,3 +11,11 @@ class SimpleBody(Body):
         super().__init__()
         self.name = 'Simple Body'
         self.hp = 2
+
+
+class HardBody(Body):
+
+    def __init__(self):
+        super().__init__()
+        self.name = 'Hard Body'
+        self.hp = 5
--- test_body.py
+++ test_body.py
@@ -6,3 +6,10 @@ def test_simple_body_attributes():
     assert simple_body.name == 'Simple Body'
     assert simple_body.hp == 2
     assert isinstance(simple_body, body.Body)
+
+
+def test_hard_body_attributes():
+    hard_body = body.HardBody()
+    assert hard_body.name == 'Hard Body'
+    assert hard_body.hp == 5
+    assert isinstance(hard_body, body.Body)
