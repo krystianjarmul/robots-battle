30eb6bee4d0269efc5a9063de08d72859e9ef2fb
Krystian Jarmul <krystian.jarmul@ericsson.com>
2021-02-21 10:28:34 +0100

    Add deactivated robot class

--- robot.py
+++ robot.py
@@ -10,11 +10,21 @@ class Team(enum.Enum):
 
 
 class Robot:
+    hp: int
+
+
+class ActivatedRobot(Robot):
 
     def __init__(self, team: Team):
         self.team = team
         self.bodies: List[body.Body] = [body.SimpleBody(), ]
         self.weapons: List[weapon.Weapon] = [weapon.BasicShot(), ]
-        self.hp = 2
-        self.movement = 1
-        self.weapon_slots = 1
+        self.hp: int = 2
+        self.movement: int = 1
+        self.weapon_slots: int = 1
+
+
+class DeactivatedRobot(Robot):
+
+    def __init__(self):
+        self.hp = 1
--- test_robot.py
+++ test_robot.py
@@ -1,17 +1,17 @@
-from robot import Robot, Team
+from robot import ActivatedRobot, Team
 import body
 import weapon
 
 
 def test_robot_belongs_to_blue_or_read_team():
-    robot1 = Robot(Team.BLUE)
-    robot2 = Robot(Team.RED)
+    robot1 = ActivatedRobot(Team.BLUE)
+    robot2 = ActivatedRobot(Team.RED)
     assert robot1.team == Team.BLUE
     assert robot2.team == Team.RED
 
 
 def test_robot_at_start_has_default_attributes():
-    robot = Robot(Team.BLUE)
+    robot = ActivatedRobot(Team.BLUE)
     assert robot.bodies == [body.SimpleBody(), ]
     assert robot.weapons == [weapon.BasicShot(), ]
     assert robot.hp == 2
