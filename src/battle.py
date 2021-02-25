98fcfa172f10db0cdd7c447fcc97f26096d5f688
Krystian Jarmul <krystian.jarmul@ericsson.com>
2021-02-25 14:12:15 +0100

    Add getting index via enumerate

--- src/battle.py
+++ src/battle.py
@@ -28,25 +28,22 @@ class Battle:
         robot.turn(direction)
 
     def attack(self, robot: ActivatedRobot, idx: int = 0):
-        # szuka czy w zasiegu razenia jest jakis robot
         weapon = robot.attack(idx)
         attack_fields = self._get_attack_fields(robot, weapon)
+
         positions_attacked = [
             field for field in attack_fields
             if self.arena.board[field[0]][field[1]] == 'x'
         ]
-        print(positions_attacked)
+
         all_robots = self.deactivated_robots + [
             self.robot_red, self.robot_blue
         ]
+
         for robot in all_robots:
             if robot.position in positions_attacked:
                 robot.hp -= 1
 
-        # jesli tak - pobiera jego polozenie i sprawdza po wszystkich robotach ktory to
-        # lista robotow jako attribute battle (deactivated + activated)
-        # jesli nie - nic sie nie dzieje
-        # nastepnie odejmuje mu 1hp
 
     def init_robots(self):
         self._set_robot(Team.RED)
@@ -57,15 +54,17 @@ class Battle:
     ) -> List[Tuple[int]]:
         facing_idx = robot.facing.index(1)
         weapon_range = get_turned_matrix(weapon.range, facing_idx)
-        weapon_drc = get_turned_matrix(weapon.directions, facing_idx)
+        weapon_direction = get_turned_matrix(weapon.directions, facing_idx)
 
         attack_attrs = [
             {
-                'x_start': robot.position[0] - 1 + weapon_drc.index(row),
-                'y_start': robot.position[1] - 1 + row.index(col),
-                'range': weapon_range[weapon_drc.index(row)][row.index(col)]
+                'x_start': robot.position[0] - 1 + row_idx,
+                'y_start': robot.position[1] - 1 + col_idx,
+                'range': weapon_range[row_idx][col_idx]
             }
-            for row in weapon_drc for col in row if col
+            for row_idx, row in enumerate(weapon_direction)
+            for col_idx, col in enumerate(row)
+            if col
         ]
 
         attack_fields = [
